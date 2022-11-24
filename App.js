d5102adc1caa93e2ba78edbd6752318ffbb59070
Matt Lee <matthew.d.lee@hotmail.co.uk>
2022-11-24 10:46:02 +0000

    Expo-Sensors

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Accel from './components/Accelerometer'
+import Gyro from './components/Gyroscope';
+import Barom from './components/Barometer';
+import Magno from './components/Magnometer';
+import Pedom from './components/Pedometer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Accel />
+      {/* <Gyro /> */}
+      {/* <Barom /> */}
+      {/* <Magno /> */}
+      {/* <Pedom /> */}
     </View>
   );
 }
